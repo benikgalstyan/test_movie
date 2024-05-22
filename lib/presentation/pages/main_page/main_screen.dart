29179225f127be76552f5ef5fd3689cac324f68b
Benik <galstyan.benik@gmail.com>
2024-05-23 01:55:00 +0300

    implemented main layout

--- lib/presentation/pages/main_page/main_screen.dart
+++ lib/presentation/pages/main_page/main_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:test_movie/presentation/pages/main_page/main_layout.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MainLayout();
   }
 }
